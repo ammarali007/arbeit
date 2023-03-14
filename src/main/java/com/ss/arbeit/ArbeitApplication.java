4ee12abcedd352a463256e8ef2db895039c3f2cd
Aamir Latif <aamirlatif@gmail.com>
2023-03-14 19:41:11 +0100

    Added initial structure

---
+++ src/main/java/com/ss/arbeit/ArbeitApplication.java
@@ -0,0 +1,13 @@
+package com.ss.arbeit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbeitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbeitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ss/arbeit/ArbeitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.arbeit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbeitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
